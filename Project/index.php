81e79367465cdb81cf1c70ac88f9b24f5e8b9da2
Faizdziaulhaq18 <91351921+Faizdziaulhaq18@users.noreply.github.com>
2022-04-22 22:11:00 +0700

    Add files via upload

---
+++ Project/index.php
@@ -0,0 +1,59 @@
+<?php
+include_once 'header.php';
+include_once 'sidebar.php';
+?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Daftar Isi</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="#">Layout</a></li>
+              <li class="breadcrumb-item active">Fixed Layout</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Ini Adalah Project 1</h3>
+
+                <div class="card-tools">
+                </div>
+              </div>
+              <div class="card-body">
+                <ol class="list-group">
+                  <li class="list-group-item">Praktikum 1 : Membuat array <a href="praktikum1.php">Data Nilai Siswa</a></li>
+                  <li class="list-group-item">Praktikum 2 : Membuat <a href="praktikum2.1.php">Form Belanja Online</a> menggunakan request post dan <a href="bd.php">Form Nilai</a> menggunakan control logika</li>
+                  <li class="list-group-item">Praktikum 3 : Mengaplikasikan <a href="praktikum3.php">Footer & Header</a> pada form nilai siswa</li>
+                  <li class="list-group-item">Praktikum 4 : Membuat <a href="praktikum4.1.php">Class Persegi Panjang</a></li>
+                  <li class="list-group-item">Praktikum 5 : Membuat <a href="praktikum5.php">Class Dispenser</a></li>
+              </ol>
+              </div>
+              <!-- /.card-body -->
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+<?php
+include_once 'footer.php';
+?>
